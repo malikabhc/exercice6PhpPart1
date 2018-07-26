477ebe9b31430b0646f482a005299953cf8ced72
Malikabhc <malika.bouhcine@gmail.com>
2018-07-26 13:39:11 +0200

    exercice6

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 6</title>
+  </head>
+  <body>
+    <?php
+      $name = 'Malika';
+      echo 'Bonjour ' . $name . ' comment vas-tu ?';
+    ?>
+  </body>
+</html>
